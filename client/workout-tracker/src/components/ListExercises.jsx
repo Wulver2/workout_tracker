b90e9155456cd9ce63e9d46eb472f42564a49623
Wulver2 <ross.shayla21@gmail.com>
2026-05-13 09:13:55 -0400

    Render exercise data on screen exercise data now appears on home screen. future use once the database is larger, will be to use it for browsing and filtering for exercises to add to workouts

--- client/workout-tracker/src/components/ListExercises.jsx
+++ client/workout-tracker/src/components/ListExercises.jsx
@@ -1,23 +1,36 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const ListExercises = () => {
+    const [exercises, setExercises] = useState([]);
+
     const getExercises = async() => {
         try {
             const response = await fetch("http://localhost:8080/exercises");
             const jsonData = await response.json();
 
-            console.log(jsonData)
+            setExercises(jsonData)
         } catch (err) {
             console.error(err.message);
         }
     }
     useEffect(() => {
         getExercises();
-    });
+    }, []);
 
     return (
     <Fragment>
         <h1>Exercises</h1>
+        <table>
+            <tbody>
+                {exercises.map(exercise => (
+                    <tr key={exercise.exercise_id}>
+                        <td>{exercise.name}</td>
+                        <td>{exercise.muscle_groups}</td>
+                        <td>{exercise.equipment}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     </Fragment>
     );
 };
