202eb21ed812f26f2ebf9022d677a8e59ae33b88
Wulver2 <ross.shayla21@gmail.com>
2026-05-18 10:19:16 -0400

    make link for exercise page back to homepage

--- client/workout-tracker/src/pages/exercises.jsx
+++ client/workout-tracker/src/pages/exercises.jsx
@@ -1,9 +1,11 @@
 import { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
 import ListExercises from "../components/ListExercises";
 
 export function Exercises() {
   return (
     <Fragment>
+      <Link to="/">Home</Link>
        <ListExercises  />
     </Fragment>
   );
