ce977d4e95a4c0c417a5ab5cf556c132fba4978f
Wulver2 <ross.shayla21@gmail.com>
2026-05-21 08:45:34 -0400

    add drop down for exercise selection

--- client/workout-tracker/src/components/CreateWorkout.jsx
+++ client/workout-tracker/src/components/CreateWorkout.jsx
@@ -1,7 +1,10 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 // Creating a Workout will add new objcts to exercise sets
 const CreateWorkout = () => {
+    // for exercise form drop down
+    const [availableExercises, setAvailableExercises] = useState([]);
+
     const currentDate = new Date();
     const defaultDate = (currentDate.getMonth + 1) + "/" +
                         currentDate.getDate + "/" + currentDate.getFullYear;
@@ -10,6 +13,21 @@ const CreateWorkout = () => {
     const [exercises, setExercises] = useState([]);
     const [date, setDate] = useState(defaultDate);
 
+    //Send query to get exercises
+    const getAvailableExercises = async() => {
+        try {
+            const res = await fetch("http://localhost:8080/exercises");
+            const jsonData = await res.json();
+
+            setAvailableExercises(jsonData);
+        } catch (err) {
+            console.error(err.message)
+        }
+    }; 
+
+    useEffect(()=> {
+        getAvailableExercises();
+    },[]);
     // TODO: Find way to implement exercise array so that
     // when an exercise is selected, queries user for sets,
     // reps, etc
@@ -17,14 +35,21 @@ const CreateWorkout = () => {
         <Fragment>
             <h1>Create a new Workout</h1>
             <form>
-                <label htmlFor="workout_name">Name of Workout</label>
-                <input type="text" id="workout_name"/>
-
-                <label htmlFor="exercises_array">Exercises</label>
-                <input type="" id="exercises_array" />
+                <label htmlFor="workout_name">Name of Workout: </label>
+                <input type="text" id="workout_name" value={name}/>
 
                 <label htmlFor="workout_date">date</label>
-                <input type="date" id="workout_date"/>
+                <input type="date" id="workout_date" value={date}/>
+
+                <label htmlFor="exercises_array">Exercises</label>
+
+                {/*dropdown */}
+                <select name="" id="">
+                   {availableExercises.map(ex => (
+                    <option value={ex.exercise_id}>{ex.name}</option>
+                   ))}
+                </select>
+
                 <button>Submit</button>
             </form>
         </Fragment>
