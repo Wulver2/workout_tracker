dad36a2fecf6f9b2bda65764e7e657047f8d3484
Wulver2 <ross.shayla21@gmail.com>
2026-06-01 09:55:47 -0400

    Format header into a row rather than a centered column

--- client/workout-tracker/src/components/Navbar.jsx
+++ client/workout-tracker/src/components/Navbar.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom"
 export function Navbar() {
     return (
         <>
-            <Link to="/">Home</Link>
-            <Link to="/exercises">Browse Exercises</Link>
-            <Link to="/new_Workout"> Create New Workout</Link>
-            <Link to="/current_workouts">Current Workouts</Link>
+            <div id="header">
+                <Link to="/">Home</Link>
+                <Link to="/exercises">Browse Exercises</Link>
+                <Link to="/new_Workout"> Create New Workout</Link>
+                <Link to="/current_workouts">Current Workouts</Link>
+            </div>
         </>
     )
 }
