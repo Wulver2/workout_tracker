7023a091d6d6ad5872d802bea0e6ac803c386e3a
Wulver2 <ross.shayla21@gmail.com>
2026-07-05 10:52:17 -0400

    Create table for users in database

--- server/database.sql
+++ server/database.sql
@@ -41,4 +41,13 @@ CREATE TABLE exercise_sets (
     
 );
 ALTER TABLE exercise_sets ADD session_id INT REFERENCES workout_session(session_id);
+-- user id linked to workout_sessions
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
