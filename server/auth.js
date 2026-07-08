36d6c5ac15be4985bf475a001928a37bb07a6450
Wulver2 <ross.shayla21@gmail.com>
2026-07-08 10:15:25 -0400

    Generate access token for new users

--- server/auth.js
+++ server/auth.js
@@ -1,4 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const pool = require("./db");
+
+
+// create account
+app.post('/register', (req, res) => {
+    const {first_name, last_name, email, password} = req.body;
+
+    // check that all fields are defined
+    // check if user exists 
+    // hash password before storing
+
+    // generate token
+    const accessToken = jwt.sign(
+        user, 
+        process.env.ACCESS_JWT_SECRET, 
+        {expiresIn: "30d"});
+
+    res.json({accessToken : accessToken})
+
+})
+
+app.listen(8080)
