98aac75ef188d345004a4d07a5f8ab7b367fbb2e
Wulver2 <ross.shayla21@gmail.com>
2026-07-14 09:26:02 -0400

    Create http-only cookie for refreshToken on login/register

--- server/auth.js
+++ server/auth.js
@@ -6,11 +6,11 @@ const pool = require("./db");
 
 
 // create account
-app.post('/register', async(req, res) => {
-    const {first_name, last_name, email, password} = req.body;
+app.post('/register', async (req, res) => {
+    const { first_name, last_name, email, password } = req.body;
 
     // check that all fields are defined
-    if (!first_name || !last_name || !email|| !password) {
+    if (!first_name || !last_name || !email || !password) {
         return res.status(400)
     }
     // check if user exists 
@@ -26,27 +26,33 @@ app.post('/register', async(req, res) => {
     const registerUser = await pool.query(`
         INSERT INTO users (first_name, last_name, email , password)
         VALUES ($1, $2, $3, $4)`, [
-            first_name,
-            last_name,
-            email,
-            hashedPw
-        ]);
+        first_name,
+        last_name,
+        email,
+        hashedPw
+    ]);
     // generate token
     //store in http-only cookie
     const refreshToken = jwt.sign(
-        registerUser.rows[0].id, 
-        process.env.REFRESH_JWT_TOKEN, 
-        {expiresIn: "30d"});
+        registerUser.rows[0].id,
+        process.env.REFRESH_JWT_TOKEN,
+        { expiresIn: "30d" });
 
     const accessToken = jwt.sign(
-        registerUser.rows[0].id, 
-        process.env.ACCESS_JWT_TOKEN, 
-        {expiresIn: "15m"});
+        registerUser.rows[0].id,
+        process.env.ACCESS_JWT_TOKEN,
+        { expiresIn: "15m" });
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: "strict",
+    })
 
 })
 
-app.post('/login', async(req, res) => {
-    const {email, password} = req.body;
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
     if (!email || !password) {
         return res.status(400);
@@ -61,16 +67,22 @@ app.post('/login', async(req, res) => {
     if (!isMatch) {
         return res.status(400);
     }
-    
+
     const refreshToken = jwt.sign(
-        user.rows[0].id, 
-        process.env.REFRESH_JWT_TOKEN, 
-        {expiresIn: "30d"});
+        user.rows[0].id,
+        process.env.REFRESH_JWT_TOKEN,
+        { expiresIn: "30d" });
 
     const accessToken = jwt.sign(
-        user.rows[0].id, 
-        process.env.ACCESS_JWT_TOKEN, 
-        {expiresIn: "15m"});
+        user.rows[0].id,
+        process.env.ACCESS_JWT_TOKEN,
+        { expiresIn: "15m" });
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: "strict",
+    })
 
 })
 
